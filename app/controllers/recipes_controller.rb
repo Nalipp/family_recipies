d5c2ce7cc23335c63347c09c08af0df857573e84
Nalipp <nateplipp@gmail.com>
2015-10-28 16:49:22 +0900

    add edit recipe functions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,13 +13,27 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(2)
+    @recipe.chef = Chef.find(3)
 
     if @recipe.save
       flash[:success] = "Your recipe was created successfully!"
       redirect_to recipes_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
     end
   end
 
