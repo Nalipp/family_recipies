44e797a256b8c7ef3c9d428dfe8144884a777d19
Nalipp <nateplipp@gmail.com>
2015-11-01 17:43:11 +0900

    create registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
   root   'pages#home'
-
-  get    '/home',             to: 'pages#home'
+  get    '/home',      to: 'pages#home'
 
   resources :recipes do
     member do
       post "like"
     end
   end
+
+  resources :chefs, except: [:new]
+  get '/register', to: "chefs#new"
 
 end
